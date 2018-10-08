84fd5d38049178c1ee5ed452612db1e8066cf8fd
RAISA TASNIM <raisatasnim19@gmail.com>
2018-10-08 14:08:15 +0600

    tkn change v5

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 
 const token = process.env.token;
-//const token = botconfig.token;
 
 const bot = new Discord.Client({disableEveryone: true});
 
